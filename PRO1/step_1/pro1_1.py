f0eee49d8543ab235cb8a539f21a45daa85c5ab5
jonahaag <jona.haag99@googlemail.com>
2022-10-06 10:43:32 +0200

    Add excel table with critical count as quantitative measure for step 1, adjust pro1_1.py accordingly

--- PRO1/step_1/pro1_1.py
+++ PRO1/step_1/pro1_1.py
@@ -23,14 +23,31 @@ switch_positions = [[False, False, False],[True, False, False],[False, True, Fal
 for l in lines:
     net.line.loc[l, "in_service"] = False
     for switch_position in switch_positions:
-        print(f"S1 {switch_position[0]}, S2 {switch_position[1]}, S3 {switch_position[2]}, Max voltage {net.res_bus.vm_pu.max()}, Min voltage {net.res_bus.vm_pu.min()}, Max line loading {net.res_line.loading_percent.max()}")
         # S1 is 4, S2 is 1, S3 is 2
         net.switch.loc[4, "closed"] = switch_position[0]
         net.switch.loc[1, "closed"] = switch_position[1]
         net.switch.loc[2, "closed"] = switch_position[2]
         pp.runpp(net)
-        if net.res_bus.vm_pu.max() > vmax or net.res_bus.vm_pu.min() < vmin or net.res_line.loading_percent.max() > max_ll:
-            critical.append([l,switch_position])
+        print(f"S1 {switch_position[0]}, S2 {switch_position[1]}, S3 {switch_position[2]}, Max voltage {net.res_bus.vm_pu.max()}, Min voltage {net.res_bus.vm_pu.min()}, Max line loading {net.res_line.loading_percent.max()}")
+        n_critical_lines = 0
+        n_overloaded_lines = 0
+        n_critical_buses = 0
+        n_very_critical_buses = 0
+        for line in net.res_line.loading_percent:
+            if line > 95.0:
+                n_critical_lines += 1
+            if line > 100.0:
+                n_overloaded_lines += 1
+        for bus in net.res_bus.vm_pu:
+            if bus < 0.95:
+                n_critical_buses += 1
+            if bus < 0.9:
+                n_very_critical_buses += 1
+        print(f"{n_critical_lines} lines over 95% loading, {n_overloaded_lines} lines over 100% loading")
+        print(f"{n_critical_buses} buses under 0.95 voltage, {n_very_critical_buses} buses under 0.90 voltage")
+        # if net.res_bus.vm_pu.max() > vmax or net.res_bus.vm_pu.min() < vmin or net.res_line.loading_percent.max() > max_ll:
+        #     critical.append([l,switch_position])
+        # lines over 95% / 100% ll, buses under 0.95 / 0.9 pu
         # ax = pplt.simple_plot(net, show_plot=False)
         # clc = pplt.create_line_collection(net, critical, color="r", linewidth=3., use_bus_geodata=True)
         # pplt.draw_collections([clc], ax=ax)
@@ -39,7 +56,3 @@ for l in lines:
         input()
         # critical = list()
     net.line.loc[l, "in_service"] = True
-
-# tmp = list(map(str, critical))
-# s = '\n'.join([' ' .join((l1, l2)) for l1, l2 in zip(tmp[::2], tmp[1::2])])
-# print(s)
