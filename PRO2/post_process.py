72b51c6703289d8efc1b8f0349082d39f916e77d
jonahaag <jona.haag99@googlemail.com>
2022-12-02 15:57:38 +0100

    Restructure subplot calling, add grouping by result_type, group CHP electricity in revenue plot

--- PRO2/post_process.py
+++ PRO2/post_process.py
@@ -1,14 +1,14 @@
 import py.extend_result_csv as extend
 import py.plot_results as plot
 import os
-
+import time
 if __name__ == "__main__":
-
+    time_0 = time.time()
     save_merged_results = False
-    show_plots = False
-    save_plots = True
+    show_plots = True
+    save_plots = False
 
-    electricity_price = "M25"   # 0: normal price, P25: higher price, M25: lower price
+    electricity_price = "0"   # 0: normal price, P25: higher price, M25: lower price
     if electricity_price == "0":
         result_file = os.path.join("bofit_results","Scenario_Step1_consolidated.csv")
     elif electricity_price == "P25":
@@ -17,4 +17,5 @@ if __name__ == "__main__":
         result_file = os.path.join("bofit_results","Scenario_Step2_M25_consolidated.csv")
         
     df = extend.extend(result_file, save_merged_results)    
-    plot.plot(df, electricity_price, save_plots, show_plots)
+    plot.create_plots(df, electricity_price, save_plots, show_plots)
+    print(time.time()-time_0)
--- PRO2/py/plot_results.py
+++ PRO2/py/plot_results.py
@@ -5,7 +5,7 @@ import os
 color_map = ["#d3bcaf", "#ffd14f", "#417505", "#9fb7c9", "#5e5e5e", "#B33F40"]
 labels = ['Waste', 'Wood', 'Bio Pellet', 'Electricity', 'Bio Oil', 'DH']
   
-def plot_load_duration_stackplot(df, plots_folder, save, show):
+def plot_load_duration_stackplot(df, plots_folder, save):
     # Sort loads
     df['interval'] = 1
     df_load_sorted = df.sort_values(by=['Total Load'], ascending = False)
@@ -33,173 +33,8 @@ def plot_load_duration_stackplot(df, plots_folder, save, show):
     plt.legend(loc='upper right')
     if save:
         plt.savefig(os.path.join(plots_folder, "load_duration_stackplot.png"), dpi=300)
-    if show:
-        plt.show()
-    
-def plot_fuel_input_to_boiler(df, axes):
-    y1 = "Waste_Fuel_Input_A1[Dim 1][MW]"
-    y2 = "Wood_Fuel_Input_A1[Dim 1][MW]"
-    y3 = "Pellet_Fuel_Input_A2[Dim 1][MW]"
-    y4 = "HP Load Output [MW]"
-    y5 = "Bio Oil Load Output [MW]"
-    axes[0,0].stackplot(df["Date (CEST)"], 
-                df[y1],
-                df[y2],
-                df[y3],
-                df[y4],
-                df[y5],
-                labels=labels,
-                colors=color_map)
-    axes[0,0].xaxis.set_major_locator(ticker.MultipleLocator(2))
-    axes[0,0].xaxis.set_major_formatter(ticker.ScalarFormatter())
-    axes[0,0].set_xlim([0,23])
-    axes[0,0].set_ylim([0,None])
-    axes[0,0].set_title("Fuel input to boiler")
-    axes[0,0].set_xlabel("Time [h]")
-    axes[0,0].set_ylabel("Fuel Input [MW]")
-    return axes
 
-def plot_dh_by_fuel(df, axes):
-    y1 = "Waste_CHP_Load_Output_A1[Dim 1][MW]"
-    y2 = "Wood_CHP_Load_Output_A1[Dim 1][MW]"
-    y3 = "Pellet_CHP_Load_Output_A2[Dim 1][MW]"
-    y4 = "HP Load Output [MW]"
-    y5 = "Bio Oil Load Output [MW]"
-    axes[0,1].stackplot(df["Date (CEST)"], 
-                df[y1],
-                df[y2],
-                df[y3],
-                df[y4],
-                df[y5],
-                labels=labels,
-                colors=color_map)
-    axes[0,1].xaxis.set_major_locator(ticker.MultipleLocator(2))
-    axes[0,1].xaxis.set_major_formatter(ticker.ScalarFormatter())
-    axes[0,1].set_xlim([0,23])
-    axes[0,1].set_ylim([0,None])
-    axes[0,1].set_title("DH Supply by fuel")
-    axes[0,1].set_xlabel("Time [h]")
-    axes[0,1].set_ylabel("DH Supply [MW]")
-    return axes
-
-def plot_electricity_production_by_fuel(df, axes):
-    y1 = "Waste_CHP_Electricity_Output_A1[Dim 1][MW]"
-    y2 = "Wood_CHP_Electricity_Output_A1[Dim 1][MW]"
-    y3 = "Pellet_CHP_Electricity_Output_A2[Dim 1][MW]"
-
-    width = 0.5
-    axes[1,0].bar(df["Date (CEST)"], df[y1], width, label=labels[0], color=color_map[0])
-    axes[1,0].bar(df["Date (CEST)"], df[y2], width, bottom=df[y1], label=labels[1], color=color_map[1])
-    axes[1,0].bar(df["Date (CEST)"], df[y3], width, bottom=df[y1]+df[y2], label=labels[2], color=color_map[2])
-    axes[1,0].xaxis.set_major_locator(ticker.MultipleLocator(2))
-    axes[1,0].xaxis.set_major_formatter(ticker.ScalarFormatter())
-    axes[1,0].set_xlim([-0.5,23.5])
-    axes[1,0].set_ylim([0,(df[y1]+df[y2]+df[y3]).max()*1.1])
-    axes[1,0].set_title("Electricity production by fuel")
-    axes[1,0].set_xlabel("Time [h]")
-    axes[1,0].set_ylabel("Electricity supply [MW]")
-    return axes
-
-def plot_fuel_cost(df, axes, electricity_price):
-    electricity_price_label = "Retail Price " + electricity_price
-    y1 = "Waste_Fuel_Input_A1[Dim 1][MW]"
-    y2 = "Wood_Fuel_Input_A1[Dim 1][MW]"
-    y3 = "Pellet_Fuel_Input_A2[Dim 1][MW]"
-    y4 = "HP Electricity Consumption [MW]"
-    y5 = "Bio Oil Fuel Input [MW]"
-    axes[1,1].stackplot(df["Date (CEST)"], 
-                df[y1].multiply(df["Waste Price"]),
-                df[y2].multiply(df["Wood Price"]),
-                df[y3].multiply(df["Bio Pellet Price"]),
-                df[y4].multiply(df[electricity_price_label]),
-                df[y5].multiply(df["Bio Oil Price"]),
-                labels=labels,
-                colors=color_map)
-    axes[1,1].xaxis.set_major_locator(ticker.MultipleLocator(2))
-    axes[1,1].xaxis.set_major_formatter(ticker.ScalarFormatter())
-    axes[1,1].set_xlim([0,23])
-    axes[1,1].set_ylim([0,None])
-    axes[1,1].set_title("Fuel cost")
-    axes[1,1].set_xlabel("Time [h]")
-    axes[1,1].set_ylabel("Costs [SEK]")
-    return axes
-
-def create_subplots(df, electricity_price, plots_folder, save, show):
-    fig, axes = plt.subplots(2, 2)
-    # plt.rc('legend', fontsize="x-small")
-    axes = plot_fuel_input_to_boiler(df, axes)
-    axes = plot_dh_by_fuel(df, axes)
-    axes = plot_electricity_production_by_fuel(df, axes)
-    axes = plot_fuel_cost(df,axes,electricity_price)
-
-    lines_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
-    lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
-    fig.legend(lines[0:5], labels[0:5], ncol=5, loc='center')
-    plt.tight_layout()
-    if save:
-        fig.set_size_inches((11,7), forward=False)
-        fig.savefig(os.path.join(plots_folder,f"results_{df[df.columns[0]].iloc[0][0:10]}.png"), dpi=500)
-    if show:
-        plt.show()
-
-def plot_cost_revenue(df, electricity_price, plots_folder, save, show):
-    retail_label = "Retail Price " + electricity_price
-    spot_label = "Spot Price " + electricity_price
-
-    waste_cost = df["Waste_Fuel_Input_A1[Dim 1][MW]"].multiply(df["Waste Price"]).sum()
-    wood_cost = df["Wood_Fuel_Input_A1[Dim 1][MW]"].multiply(df["Wood Price"]).sum()
-    pellet_cost = df["Pellet_Fuel_Input_A2[Dim 1][MW]"].multiply(df["Bio Pellet Price"]).sum()
-    electricity_cost = df["HP Electricity Consumption [MW]"].multiply(df[retail_label]).sum()
-    oil_cost = df["Bio Oil Fuel Input [MW]"].multiply(df["Bio Oil Price"]).sum()
-
-    waste_revenue = df["Waste_CHP_Electricity_Output_A1[Dim 1][MW]"].multiply(df[spot_label]).sum()
-    wood_revenue = df["Wood_CHP_Electricity_Output_A1[Dim 1][MW]"].multiply(df[spot_label]).sum()
-    pellet_revenue = df["Pellet_CHP_Electricity_Output_A2[Dim 1][MW]"].multiply(df[spot_label]).sum()
-    dh_revenue = df["Total Load"].multiply(df["DH Price"]).sum()
-
-    width = .8
-    fig, ax = plt.subplots()
-
-    bottom = 0
-    # for i, cost in enumerate([waste_cost, wood_cost, pellet_cost, electricity_cost, oil_cost]):
-    costs = sorted(list(zip([waste_cost, wood_cost, pellet_cost, electricity_cost, oil_cost],range(5))),reverse=True)
-    for cost, i in costs:
-        ax.bar("Cost", cost, width=width, bottom=bottom, label=labels[i], color=color_map[i])
-        bottom += cost
-    
-    bottom = 0
-    # for i, revenue in enumerate([waste_revenue, wood_revenue, pellet_revenue, dh_revenue]):
-    revenues = sorted(list(zip([waste_revenue, wood_revenue, pellet_revenue, dh_revenue],range(4))),reverse=True)
-    for revenue, i in revenues:
-        ax.bar("Revenue", revenue, width=width, bottom=bottom, label=labels[5] if i == 3 else "", color=color_map[5] if i ==3 else color_map[i])
-        bottom += revenue
-
-    # ax.set_xlabel("Time [%]")
-    ax.set_ylabel("Money [SEK]")
-    ax.legend(loc='upper left')
-    if save:
-        plt.savefig(os.path.join(plots_folder,f"costs_{df[df.columns[0]].iloc[0][0:10]}.png"), dpi=300)
-    if show:
-        plt.show()
-
-def plot_prices(df, electricity_price, plots_folder, save, show):
-    plt.subplot(111)
-    for i, price in enumerate(["Waste Price", "Wood Price", "Bio Pellet Price", "Retail Price " + electricity_price, "Bio Oil Price", "Spot Price " + electricity_price]):
-        plt.plot(df["Date (CEST)"], df[price], label=price, color=color_map[i])
-    ax = plt.gca()
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
-    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
-    ax.set_xlim([0,23])
-    ax.set_ylim([0,None])
-    ax.set_xlabel("Time [h]")
-    ax.set_ylabel("Price [SEK/kWh]")
-    plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", ncol=3)
-    if save:
-        plt.savefig(os.path.join(plots_folder,f"prices_{df[df.columns[0]].iloc[0][0:10]}.png"), dpi=300)
-    if show:
-        plt.show()
-
-def plot_chp_use(df, plots_folder, save, show):
+def plot_chp_use(df, plots_folder, save):
     y = ["Waste_Turbine_Load_A1[Dim 1][MW]", "Wood_Turbine_Load_A1[Dim 1][MW]", "Pellet_Turbine_Load_A2[Dim 1][MW]",
         "Waste_BP_Load_A1[Dim 1][MW]", "Wood_BP_Load_A1[Dim 1][MW]", "Pellet_BP_Load_A2[Dim 1][MW]"]
     fig, axes = plt.subplots(3,1)
@@ -217,10 +52,8 @@ def plot_chp_use(df, plots_folder, save, show):
     plt.tight_layout()
     if save:
         plt.savefig(os.path.join(plots_folder,"chp_use.png"), dpi=300)
-    if show:
-        plt.show()
 
-def plot_chp_use_bar(df, plots_folder, save, show):
+def plot_chp_use_bar(df, plots_folder, save):
     fig, ax = plt.subplots()
     width = 0.8
     ax.bar("Waste", df["Waste_Turbine_Load_A1[Dim 1][MW]"].sum(), width=width, label="Turbine", color=color_map[0])
@@ -233,25 +66,219 @@ def plot_chp_use_bar(df, plots_folder, save, show):
     ax.set_ylabel("Load [MW]")
     if save:
         plt.savefig(os.path.join(plots_folder,"chp_use_bar.png"), dpi=300)
-    if show:
-        plt.show()
 
-def plot(df, electricity_price, save, show):
+def plot_fuel_input(df, ax, title):
+    y1 = "Waste_Fuel_Input_A1[Dim 1][MW]"
+    y2 = "Wood_Fuel_Input_A1[Dim 1][MW]"
+    y3 = "Pellet_Fuel_Input_A2[Dim 1][MW]"
+    y4 = "HP Load Output [MW]"
+    y5 = "Bio Oil Load Output [MW]"
+    ax.stackplot(df["Date (CEST)"], 
+                df[y1],
+                df[y2],
+                df[y3],
+                df[y4],
+                df[y5],
+                labels=labels,
+                colors=color_map)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax.set_xlim([0,23])
+    ax.set_ylim([0,None])
+    ax.set_title(title)
+    ax.set_xlabel("Time [h]")
+    ax.set_ylabel("Fuel Input [MW]")
+
+def plot_dh_by_fuel(df, ax, title):
+    y1 = "Waste_CHP_Load_Output_A1[Dim 1][MW]"
+    y2 = "Wood_CHP_Load_Output_A1[Dim 1][MW]"
+    y3 = "Pellet_CHP_Load_Output_A2[Dim 1][MW]"
+    y4 = "HP Load Output [MW]"
+    y5 = "Bio Oil Load Output [MW]"
+    ax.stackplot(df["Date (CEST)"], 
+                df[y1],
+                df[y2],
+                df[y3],
+                df[y4],
+                df[y5],
+                labels=labels,
+                colors=color_map)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax.set_xlim([0,23])
+    ax.set_ylim([0,None])
+    ax.set_title(title)
+    ax.set_xlabel("Time [h]")
+    ax.set_ylabel("DH Supply [MW]")
+
+def plot_electricity_production_by_fuel(df, ax, title):
+    y1 = "Waste_CHP_Electricity_Output_A1[Dim 1][MW]"
+    y2 = "Wood_CHP_Electricity_Output_A1[Dim 1][MW]"
+    y3 = "Pellet_CHP_Electricity_Output_A2[Dim 1][MW]"
+
+    width = 0.5
+    ax.bar(df["Date (CEST)"], df[y1], width, label=labels[0], color=color_map[0])
+    ax.bar(df["Date (CEST)"], df[y2], width, bottom=df[y1], label=labels[1], color=color_map[1])
+    ax.bar(df["Date (CEST)"], df[y3], width, bottom=df[y1]+df[y2], label=labels[2], color=color_map[2])
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax.set_xlim([-0.5,23.5])
+    ax.set_ylim([0,(df[y1]+df[y2]+df[y3]).max()*1.1])
+    ax.set_title(title)
+    ax.set_xlabel("Time [h]")
+    ax.set_ylabel("Electricity supply [MW]")
+
+def plot_fuel_cost(df, ax, electricity_price, title):
+    electricity_price_label = "Retail Price " + electricity_price
+    y1 = "Waste_Fuel_Input_A1[Dim 1][MW]"
+    y2 = "Wood_Fuel_Input_A1[Dim 1][MW]"
+    y3 = "Pellet_Fuel_Input_A2[Dim 1][MW]"
+    y4 = "HP Electricity Consumption [MW]"
+    y5 = "Bio Oil Fuel Input [MW]"
+    ax.stackplot(df["Date (CEST)"], 
+                df[y1].multiply(df["Waste Price"]),
+                df[y2].multiply(df["Wood Price"]),
+                df[y3].multiply(df["Bio Pellet Price"]),
+                df[y4].multiply(df[electricity_price_label]),
+                df[y5].multiply(df["Bio Oil Price"]),
+                labels=labels,
+                colors=color_map)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax.set_xlim([0,23])
+    ax.set_ylim([0,None])
+    ax.set_title(title)
+    ax.set_xlabel("Time [h]")
+    ax.set_ylabel("Costs [SEK]")
+
+def pick_position_of_subplot(i):
+    if i == 0:
+        return 0, 0
+    elif i == 1:
+        return 0,1
+    elif i == 2:
+        return 1,0
+    elif i == 3:
+        return 1,1
+    
+def plot_subplots(df, electricity_price, plots_folder, save):
+    fig_1, axes_1 = plt.subplots(2,2)
+    fig_2, axes_2 = plt.subplots(2,2)
+    fig_3, axes_3 = plt.subplots(2,2)
+    fig_4, axes_4 = plt.subplots(2,2)
+    fig_5, axes_5 = plt.subplots(2,2)
+    fig_6, axes_6 = plt.subplots(2,2)
+    fig_7, axes_7 = plt.subplots(2,2)
+    fig_8, axes_8 = plt.subplots(2,2)
+    figs = [fig_1, fig_2, fig_3, fig_4, fig_5, fig_6 , fig_7, fig_8]
+    axes = [axes_1, axes_2, axes_3, axes_4, axes_5, axes_6, axes_7, axes_8]
+
+    for i, day_start_id in enumerate([1392, 4727, 7968, 2255]):
+        df_day = df.iloc[day_start_id:day_start_id+24]
+        day_string = str(df_day[df_day.columns[0]].iloc[0][0:10])
+        j, k = pick_position_of_subplot(i)
+
+        plot_fuel_input(df_day, axes[i][0,0], title="Fuel input to boiler")
+        plot_fuel_input(df_day, axes[4][j,k], title=day_string)
+
+        plot_dh_by_fuel(df_day, axes[i][0,1], title="DH Supply by fuel")
+        plot_dh_by_fuel(df_day, axes[5][j,k], title=day_string)
+
+        plot_electricity_production_by_fuel(df_day, axes[i][1,0], title="Electricity production by fuel")
+        plot_electricity_production_by_fuel(df_day, axes[6][j,k], title=day_string)
+
+        plot_fuel_cost(df_day, axes[i][1,1], electricity_price, title="Fuel Cost")
+        plot_fuel_cost(df_day, axes[7][j,k], electricity_price, title=day_string)
+
+        lines_labels = [ax.get_legend_handles_labels() for ax in figs[i].axes]
+        lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
+        figs[i].legend(lines[0:5], labels[0:5], ncol=5, loc='center')
+        figs[i].tight_layout()
+        if save:
+            figs[i].set_size_inches((11,7), forward=False)
+            figs[i].savefig(os.path.join(plots_folder,f"results_{day_string}.png"), dpi=500)
+        
+        plot_cost_revenue(df_day, electricity_price, plots_folder, save)
+        plot_prices(df_day, electricity_price, plots_folder, save)
+
+    for i, result_type in enumerate(["fuel_input", "dh_by_fuel", "electricity_production", "fuel_cost"]):
+        lines_labels = [ax.get_legend_handles_labels() for ax in figs[i+4].axes]
+        lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
+        if i == 2:
+            figs[i+4].legend(lines[0:3], labels[0:3], ncol=3, loc='center')
+        else:
+            figs[i+4].legend(lines[0:5], labels[0:5], ncol=5, loc='center')
+        figs[i+4].suptitle(result_type)
+        figs[i+4].tight_layout()
+        if save:
+            figs[i+4].set_size_inches((11,7), forward=False)
+            figs[i+4].savefig(os.path.join(plots_folder,f"results_{result_type}_all_days.png"), dpi=500)
+
+def plot_cost_revenue(df, electricity_price, plots_folder, save):
+    retail_label = "Retail Price " + electricity_price
+    spot_label = "Spot Price " + electricity_price
+
+    waste_cost = df["Waste_Fuel_Input_A1[Dim 1][MW]"].multiply(df["Waste Price"]).sum()
+    wood_cost = df["Wood_Fuel_Input_A1[Dim 1][MW]"].multiply(df["Wood Price"]).sum()
+    pellet_cost = df["Pellet_Fuel_Input_A2[Dim 1][MW]"].multiply(df["Bio Pellet Price"]).sum()
+    electricity_cost = df["HP Electricity Consumption [MW]"].multiply(df[retail_label]).sum()
+    oil_cost = df["Bio Oil Fuel Input [MW]"].multiply(df["Bio Oil Price"]).sum()
+
+    waste_revenue = df["Waste_CHP_Electricity_Output_A1[Dim 1][MW]"].multiply(df[spot_label]).sum()
+    wood_revenue = df["Wood_CHP_Electricity_Output_A1[Dim 1][MW]"].multiply(df[spot_label]).sum()
+    pellet_revenue = df["Pellet_CHP_Electricity_Output_A2[Dim 1][MW]"].multiply(df[spot_label]).sum()
+    electricity_revenue = waste_revenue + wood_revenue + pellet_revenue
+    dh_revenue = df["Total Load"].multiply(df["DH Price"]).sum()
+
+    width = .8
+    fig, ax = plt.subplots()
+
+    bottom = 0
+    # for i, cost in enumerate([waste_cost, wood_cost, pellet_cost, electricity_cost, oil_cost]):
+    costs = sorted(list(zip([waste_cost, wood_cost, pellet_cost, electricity_cost, oil_cost],range(5))),reverse=True)
+    for cost, i in costs:
+        ax.bar("Cost", cost, width=width, bottom=bottom, label=labels[i], color=color_map[i])
+        bottom += cost
+    
+    bottom = 0
+    # for i, revenue in enumerate([waste_revenue, wood_revenue, pellet_revenue, dh_revenue]):
+    revenues = sorted(list(zip([electricity_revenue, dh_revenue],range(2))),reverse=True)
+    for revenue, i in revenues:
+        ax.bar("Revenue", revenue, width=width, bottom=bottom, label="" if i==0 else labels[5], color=color_map[i*2+3])
+        bottom += revenue
+
+    # ax.set_xlabel("Time [%]")
+    ax.set_ylabel("Money [SEK]")
+    ax.legend(loc='upper left')
+    if save:
+        plt.savefig(os.path.join(plots_folder,f"costs_{df[df.columns[0]].iloc[0][0:10]}.png"), dpi=300)
+
+def plot_prices(df, electricity_price, plots_folder, save):
+    plt.subplot(111)
+    for i, price in enumerate(["Waste Price", "Wood Price", "Bio Pellet Price", "Retail Price " + electricity_price, "Bio Oil Price", "Spot Price " + electricity_price]):
+        plt.plot(df["Date (CEST)"], df[price], label=price, color=color_map[i])
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax.set_xlim([0,23])
+    ax.set_ylim([0,None])
+    ax.set_xlabel("Time [h]")
+    ax.set_ylabel("Price [SEK/kWh]")
+    plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", ncol=3)
+    if save:
+        plt.savefig(os.path.join(plots_folder,f"prices_{df[df.columns[0]].iloc[0][0:10]}.png"), dpi=300)
+
+def create_plots(df, electricity_price, save, show):
     plots_folder = "plots_" + electricity_price
     if not os.path.exists(plots_folder):
             os.makedirs(plots_folder)
     # Plot load duration curve
-    plot_load_duration_stackplot(df, plots_folder, save, show)
-    plot_chp_use(df, plots_folder, save, show)
-    plot_chp_use_bar(df, plots_folder, save, show)
+    plot_load_duration_stackplot(df, plots_folder, save)
 
-    # Plot special days
-    day_start_ids = [1392, 4727, 7968, 2255]
-    # for day_id in [59, 198, 333, 95]: # max load, min load, max elec. price, min elec. price
-    for day_start_id in day_start_ids:
-        # df_day = df.iloc[(day_id-1)*24:day_id*24]
-        df_day = df.iloc[day_start_id:day_start_id+24] # some weird things going on
-        # Create a plot of subplots
-        create_subplots(df_day, electricity_price, plots_folder, save, show)
-        plot_cost_revenue(df_day, electricity_price, plots_folder, save, show)
-        plot_prices(df_day, electricity_price, plots_folder, save, show)
+    plot_chp_use(df, plots_folder, save)
+    plot_chp_use_bar(df, plots_folder, save)
+
+    plot_subplots(df, electricity_price, plots_folder, save)
+    if show:
+        #TODO this is a bit messed up
+        plt.show()
